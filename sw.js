15f4d9548af0041a12d673fd8713f0fb2f6e8294
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2025-08-18 13:40:19 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='ht-cache-v3';const PRECACHE=['./','./index.html','./app.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-192.png','./icons/maskable-512.png'];
+const CACHE='ht-structured-v1';
+const PRECACHE=['./','./index.html','./css/style.css','./js/app.js','./js/ui.js','./js/storage.js','./data/routines.json','./data/defaults_1rm.json','./app.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-192.png','./icons/maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;e.respondWith(caches.match(r).then(c=>c||fetch(r).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(ca=>ca.put(r,copy));return resp}).catch(()=>caches.match('./index.html'))))});
