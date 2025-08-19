ba00019766e045d4ef2293a4d9315398e8839b42
Israel Ortiz <52337476+Barushv@users.noreply.github.com>
2025-08-18 18:06:09 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ht-structured-v1';
+const CACHE='ht-structured-v1-1'; // bump to refresh
 const PRECACHE=['./','./index.html','./css/style.css','./js/app.js','./js/ui.js','./js/storage.js','./data/routines.json','./data/defaults_1rm.json','./app.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-192.png','./icons/maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
